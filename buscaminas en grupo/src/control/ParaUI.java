79484f9a9351c87f5d04a372c5b393e32ca1ea6e
albertoobreo <59649525+albertoobreo@users.noreply.github.com>
2021-04-26 20:20:11 +0200

    botonera y bontes inicio juntos

--- buscaminas en grupo/src/control/ParaUI.java
+++ buscaminas en grupo/src/control/ParaUI.java
@@ -1,19 +1,27 @@
 package control;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+
+import modelo.Coordenada;
 import modelo.Densidad;
 import modelo.Dificultad;
+import modelo.TableroAleatorio;
 import vista.UI;
 
 public class ParaUI extends UI {
 
 	private Controlador controlador;
+	private ActionListener actionListener;
+	private TableroAleatorio tablero = new TableroAleatorio(Dificultad.medio.getLongitud(), 7);
 	
 	public ParaUI() {
 		super();
 		controlador=new Controlador();
+		this.preparameLaBotonera();
 		// leyes de demeter
 		// para solucionar esto es crear metodos delegados
 //		jPanelOpciones.btnIniciar.addActionListener(l);
@@ -26,8 +34,42 @@ public class ParaUI extends UI {
 				System.out.println(dificultad.getLongitud());
 				controlador.dameValores(densidad, dificultad);
 //				jPanelOpciones.getCmbDensidad();
+				botonera.setVisible(true);
 			}
 		});		
 	}
+	private void preparameLaBotonera() {
+		this.actionListenerBotonera();
+		this.addEventosBotonera();
+		
+	}
+	private void actionListenerBotonera() {
+		this.actionListener = new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JButton boton = (JButton) e.getSource();
+				Coordenada coordenada2 = botonera.getCoordenada(boton);
+				int minasAlrededor = tablero.getCasilla(coordenada2).getMinasAlrededor();
+				boton.setText(String.valueOf(minasAlrededor));
+				
+			}
+		};
+		
+		}
+		
+	private void addEventosBotonera() {
+		Component[]boton = this.botonera.getComponents();
+		for (Component component : boton) {
+			((JButton)component).addActionListener(this.actionListener);
+		}
+	}
+//	private void visualizaBotonera() {
+//		Component[] boton = this.botonera.getComponents();
+//		for (Component component : boton) {
+//			((JButton) component).setVisible(true);
+//		}
+		
+//	}
 
 }
